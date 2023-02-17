872f5d653ff18d5bc7b58ee1af9afcdc86d18f46
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-02-17 13:54:54 -0300

    Criação do menu de conta bancária

---
+++ src/conta/Menu.java
@@ -0,0 +1,89 @@
+package conta;
+
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+
+        int opcao;
+
+        while (true){
+
+            System.out.println("*****************************************************");
+            System.out.println("                                                     ");
+            System.out.println("                BANCO DO BRAZIL COM Z                ");
+            System.out.println("                                                     ");
+            System.out.println("*****************************************************");
+            System.out.println("                                                     ");
+            System.out.println("            1 - Criar Conta                          ");
+            System.out.println("            2 - Listar todas as Contas               ");
+            System.out.println("            3 - Buscar Conta por Numero              ");
+            System.out.println("            4 - Atualizar Dados da Conta             ");
+            System.out.println("            5 - Apagar Conta                         ");
+            System.out.println("            6 - Sacar                                ");
+            System.out.println("            7 - Depositar                            ");
+            System.out.println("            8 - Transferir valores entre Contas      ");
+            System.out.println("            9 - Sair                                 ");
+            System.out.println("                                                     ");
+            System.out.println("*****************************************************");
+            System.out.println("Entre com a opção desejada: ");
+
+            opcao = leia.nextInt();
+
+            if (opcao == 9){
+                System.out.println("Banco do Brazil com Z - O seu futuro começa aqui!");
+                leia.close();
+                System.exit(0);
+            }
+
+            switch (opcao){
+                case 1:
+                    System.out.println("Criar conta\n\n");
+
+                    break;
+
+                case 2:
+                    System.out.println("Listar todas as contas\n\n");
+
+                    break;
+
+                case 3:
+                    System.out.println("Consultar dados da conta - por número\n\n");
+
+                    break;
+
+                case 4:
+                    System.out.println("Atualizar dados da conta\n\n");
+
+                    break;
+
+                case 5:
+                    System.out.println("Apagar a conta\n\n");
+
+                    break;
+
+                case 6:
+                    System.out.println("Saque\n\n");
+
+                    break;
+
+                case 7:
+                    System.out.println("Depósito\n\n");
+
+                    break;
+
+                case 8:
+                    System.out.println("Transferência entre contas\n\n");
+
+                    break;
+
+                default:
+                    System.out.println("\nOpção inválida");
+                    break;
+            }
+        }
+
+    }
+}
